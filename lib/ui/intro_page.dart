b658aeb4c92614fdfdbda7a47844de2f17a20849
Mostafa <mohammadimostafa858@gmail.com>
2024-10-14 00:18:17 +0330

    finish intro pages

--- lib/ui/intro_page.dart
+++ lib/ui/intro_page.dart
@@ -2,24 +2,27 @@ import 'package:dailysync/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
+
   @override
   _OnboardingScreenState createState() => _OnboardingScreenState();
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   PageController _pageController = PageController();
+
   int currentPage = 0;
+
 
   final List<Map<String, String>> onboardingData = [
     {
       "image": "assets/images/intro1.png",
-      "title": "کارهات را مدیریت کن",
+      "title": "کارهات رو مدیریت کن",
       "description": "به‌راحتی تمام کارهای روزانه‌ات را در کارنامه به صورت رایگان مدیریت کن."
     },
     {
       "image": "assets/images/intro2.png",
-      "title": "پیشرفتت را دنبال کن",
-      "description": "کارهای انجام‌شده و در انتظار را به‌طور مؤثر پیگیری کن."
+      "title": "پیشرفتت رو دنبال کن",
+      "description": "کارهای انجام‌شده و در انتظار رو به‌طور مؤثر پیگیری کن."
     },
     {
       "image": "assets/images/intro3.png",
@@ -30,15 +33,24 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: primaryColor,
-        title: Text('skip',style: TextStyle(color: Colors.white.withOpacity(0.44)),),
+        backgroundColor: primaryColor, // Adjusted to match primaryColor
+        title: Text(
+          'گذر کردن',
+          style: TextStyle(
+              color: Colors.white.withOpacity(0.44),
+              fontFamily: 'Vazir',
+              fontSize: 16),
+        ),
       ),
-
       body: Column(
         children: [
-             PageView.builder(
+          Container(
+            height: screenHeight * 0.7, // Set a fixed height for the PageView
+            child: PageView.builder(
               controller: _pageController,
               onPageChanged: (int page) {
                 setState(() {
@@ -52,58 +64,65 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 description: onboardingData[index]["description"]!,
               ),
             ),
-
-
-
-            Column(
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              onboardingData.length,
+                  (index) => buildDot(index),
+            ),
+          ),
+          SizedBox(height: screenHeight*0.05),
+          Padding(
+            padding: const EdgeInsets.all(20.0), // Added padding for buttons
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    onboardingData.length,
-                        (index) => buildDot(index),
+                TextButton(
+
+                  onPressed: () {
+                    if (currentPage > 0) {
+                      _pageController.previousPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.ease,
+                      );
+                    }
+                  },
+                  child: Text(
+                    "بازگشت", // Localized to Persian
+                    style: TextStyle(color: Colors.white.withOpacity(0.44),fontFamily: 'Vazir',fontSize: 16),
                   ),
                 ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        if (currentPage > 0) {
-                          _pageController.previousPage(
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.ease,
-                          );
-                        }
-                      },
-                      child: Text(
-                        "BACK",
-                        style: TextStyle(color: Colors.white),
+                TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3.0),  // Set to 0 for sharp corners
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        if (currentPage < onboardingData.length - 1) {
-                          _pageController.nextPage(
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.ease,
-                          );
-                        } else {
-                          // Navigate to the next screen or home screen
-                        }
-                      },
-                      child: Text(
-                        currentPage == onboardingData.length - 1
-                            ? "FINISH"
-                            : "NEXT",
-                        style: TextStyle(color: Colors.purple),
-                      ),
-                    ),
-                  ],
+                    backgroundColor: MaterialStateProperty.all(buttoncolor), // Your button color
+                  ),
+
+                  onPressed: () {
+                    if (currentPage < onboardingData.length - 1) {
+                      _pageController.nextPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.ease,
+                      );
+                    } else {
+                      // Navigate to the next screen or home screen
+                    }
+                  },
+                  child: Text(
+
+                    currentPage == onboardingData.length - 1
+                        ? "پایان"  // Localized to Persian
+                        : "بعدی",  // Localized to Persian
+                    style: TextStyle(color: Colors.white,fontFamily: 'Vazir',fontSize: 16),
+                  ),
                 ),
               ],
-
+            ),
           ),
         ],
       ),
@@ -118,8 +137,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       height: 8,
       width: currentPage == index ? 20 : 8,
       decoration: BoxDecoration(
-        color: currentPage == index ? Colors.purple : Colors.grey,
-        borderRadius: BorderRadius.circular(5),
+        color: currentPage == index ? Colors.white : Colors.grey,
+        borderRadius: BorderRadius.circular(3),
       ),
     );
   }
@@ -136,30 +155,45 @@ class OnboardingContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Spacer(),
-        Image.asset(
-          image,
-          height: 300,
-        ),
-        Spacer(),
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              image,
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.9,
+              fit: BoxFit.contain,
+            ),
           ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          description,
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.white70),
-        ),
-
-      ],
+          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.rtl,
+            style: TextStyle(
+              fontSize: 32,
+              fontFamily: 'Vazir',
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.rtl,
+            style: TextStyle(
+              fontFamily: 'Vazir',
+              fontSize: 16,
+              color: Colors.white.withOpacity(0.77),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xff121212);
+const Color buttoncolor = Color(0xff8875FF);
